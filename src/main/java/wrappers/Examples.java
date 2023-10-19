a5410fc05b4082300bae18d5bc34e67a470b2700
Simon <simon@dancingcloudservices.com>
2023-10-19 14:07:38 -0600

    Day 2 Final

--- src/main/java/wrappers/Examples.java
+++ src/main/java/wrappers/Examples.java
@@ -1,5 +1,8 @@
 package wrappers;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Examples {
   public static void main(String[] args) {
 //    Object obj = 2; // performs autoboxing using Integer.valueOf...
@@ -16,5 +19,21 @@ public class Examples {
     long l1 = 99; // widening promotion
 //    Long l2 = 99; // FAILS--cannot convert and box in one go!
     Long l2 = 99L; // works
+
+    l1 = l2; // autounboxing
+
+    // NOPE, can only create collections etc. of REFERENCE types, not primitives
+//    List<int> nums = new ArrayList<>();
+    List<Integer> nums = new ArrayList<>();
+    nums.add(1); // autoboxing again :)
+    nums.add(2);
+    int x = nums.get(0); // autounboxing.
+
+    // excessive autoboxing/unboxing with large amounts of data
+    // MIGHT become a performance issue. (CPU and memory allocation/GC)
+    // IF you have an EASY route to avoid this, use it.
+    // OTHERWISE, if a profiler tells you that boxing is the cause of your
+    // unaccetable performance, then find a way to avoid it
+    // (e.g. int [] rather than List<Integer>)
   }
 }
